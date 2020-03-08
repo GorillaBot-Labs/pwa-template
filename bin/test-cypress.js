935c24c9d240d671f8dc3b64ccc11968f1f919b8
John Rake <john.rake12@gmail.com>
2020-03-08 12:30:54 -0500

    add 5 minute timeout limit on cypress tests

--- bin/test-cypress.js
+++ bin/test-cypress.js
@@ -1,9 +1,14 @@
 import child_process from 'child_process';
 import testServer from "../backend/helpers/testServer";
 
+const TIME_LIMIT = (60 * 1000) * 5; // five minutes
+
 testServer.start(3000);
 
 const args = ['cy:run'];
 const opts = {stdio: 'inherit', shell: true};
 child_process.spawn('yarn', args, opts);
 
+setTimeout(() => {
+    throw new Error('Failed to finish tests in under 5 minutes');
+}, TIME_LIMIT);
