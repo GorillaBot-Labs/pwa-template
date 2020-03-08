8cc2157dd45d8c10862ad72381a1f4cdf75e3e90
John Rake <john.rake12@gmail.com>
2020-03-08 10:54:22 -0500

    add global teardown for tests

---
+++ backend/globalTeardownTests.js
@@ -0,0 +1,2 @@
+// make sure we teardown the process and kill api server
+process.exit(0);
--- bin/test-server.js
+++ bin/test-server.js
@@ -1,6 +1,11 @@
 import child_process from 'child_process';
 
-const args = [ 'backend', '--globalSetup=./backend/globalSetupTests.js' ];
+const args = [
+    'backend',
+    '--globalSetup=./backend/globalSetupTests.js',
+    '--globalTeardown=./backend/globalTeardownTests.js',
+    '--detectOpenHandles'
+];
 
 if (process.env.NODE_ENV === "development") {
     args.push('--watch');
