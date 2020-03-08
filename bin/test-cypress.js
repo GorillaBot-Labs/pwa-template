a6372eb34ef5e8ad246b20074a57cb060050bb6c
John Rake <john.rake12@gmail.com>
2020-03-08 12:13:15 -0500

    add travis support for cypress

--- bin/test-cypress.js
+++ bin/test-cypress.js
@@ -3,7 +3,7 @@ import testServer from "../backend/helpers/testServer";
 
 testServer.start(3000);
 
-const args = [ 'cy:run' ];
-const opts = { stdio: 'inherit', shell: true };
+const args = ['cy:run'];
+const opts = {stdio: 'inherit', shell: true};
 child_process.spawn('yarn', args, opts);
 
